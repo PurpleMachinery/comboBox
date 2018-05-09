5b472893e7698092ed376437991aca4e447583de
wesley vieira de lima <wesley.vieria.lima.1999@gmail.com>
2018-05-08 22:29:18 -0300

    all

---
+++ combobox/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.cmbEstado = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridAll = new System.Windows.Forms.DataGridView();
+            this.Nome = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAll)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(58, 38);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(214, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.Location = new System.Drawing.Point(58, 64);
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(214, 20);
+            this.txtEndereco.TabIndex = 2;
+            // 
+            // cmbEstado
+            // 
+            this.cmbEstado.FormattingEnabled = true;
+            this.cmbEstado.Location = new System.Drawing.Point(58, 91);
+            this.cmbEstado.Name = "cmbEstado";
+            this.cmbEstado.Size = new System.Drawing.Size(214, 21);
+            this.cmbEstado.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(113, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Salvar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(113, 147);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Limpar";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridAll);
+            this.groupBox1.Location = new System.Drawing.Point(12, 241);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(276, 189);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridAll
+            // 
+            this.dataGridAll.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridAll.Location = new System.Drawing.Point(6, 19);
+            this.dataGridAll.Name = "dataGridAll";
+            this.dataGridAll.Size = new System.Drawing.Size(264, 150);
+            this.dataGridAll.TabIndex = 0;
+            this.dataGridAll.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridAll_CellDoubleClick);
+            // 
+            // Nome
+            // 
+            this.Nome.AutoSize = true;
+            this.Nome.Location = new System.Drawing.Point(3, 38);
+            this.Nome.Name = "Nome";
+            this.Nome.Size = new System.Drawing.Size(35, 13);
+            this.Nome.TabIndex = 7;
+            this.Nome.Text = "Nome";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 67);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Endereco";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 94);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Estado";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(113, 177);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Update";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Location = new System.Drawing.Point(58, 10);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(214, 20);
+            this.txtId.TabIndex = 12;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(113, 207);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Excluir";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 442);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Nome);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cmbEstado);
+            this.Controls.Add(this.txtEndereco);
+            this.Controls.Add(this.txtNome);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridAll)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.ComboBox cmbEstado;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label Nome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridAll;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Button button4;
+
+    }
+}
+
---
+++ combobox/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace combobox
+{
+    public partial class Form1 : Form
+    {
+        private SqlConnection conn = new SqlConnection("Password=info211;Persist Security Info=True;User ID=sa;Initial Catalog=combobox;Data Source=LAB-08-07");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String sql = "insert into clientes (nome, endereco, fk_estado) values ('" + txtNome.Text + "','" + txtEndereco.Text + "'," + cmbEstado.SelectedValue + ")";
+            SqlCommand comando = new SqlCommand(sql, conn);
+            conn.Open();
+            comando.ExecuteNonQuery();
+            conn.Close();
+            atualizar();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtEndereco.Text = "";
+            txtNome.Text = "";
+        }
+
+        private void atualizar()
+        {
+            String sql = "busca";
+            conn.Open();
+            SqlDataAdapter dd = new SqlDataAdapter(sql, conn);
+            DataSet set = new DataSet();
+            dd.Fill(set);
+            conn.Close();
+            dataGridAll.DataSource = set.Tables[0];
+            dataGridAll.Columns[0].Visible = false;
+
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String sql = "select * from estados";
+            conn.Open();
+            SqlDataAdapter data = new SqlDataAdapter(sql, conn);
+            DataSet set = new DataSet();
+            data.Fill(set);
+            conn.Close();
+            cmbEstado.DataSource = set.Tables[0];
+            cmbEstado.DisplayMember = "nome";
+            cmbEstado.ValueMember = "pk_id";
+            atualizar();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            String sql = "update clientes set nome = '" + txtNome.Text + "', endereco = '" + txtEndereco.Text + "', fk_estado = '" + cmbEstado.SelectedValue + "' where pk_id = " + txtId.Text;
+            SqlCommand comando = new SqlCommand(sql, conn);
+            conn.Open();
+            comando.ExecuteNonQuery();
+            conn.Close();
+            atualizar();
+        }
+
+        private void dataGridAll_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            txtId.Text = "" + dataGridAll.Rows[e.RowIndex].Cells[0].Value;
+            txtNome.Text = "" + dataGridAll.Rows[e.RowIndex].Cells[1].Value;
+            txtEndereco.Text = "" + dataGridAll.Rows[e.RowIndex].Cells[2].Value;
+            cmbEstado.Text = "" + dataGridAll.Rows[e.RowIndex].Cells[3].Value;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            String sql = "delete from clientes where pk_id = " + txtId.Text;
+            SqlCommand comando = new SqlCommand(sql, conn);
+            conn.Open();
+            comando.ExecuteNonQuery();
+            conn.Close();
+            atualizar();
+        }
+    }
+}
